4a3256503def0e4e695338eb725838ca3487f768
Marcelo Fabiano <marcelofabiano@hotmail.com.br>
2020-01-29 13:04:17 -0300

    config body-parser

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const hbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 //const Sequelize = require('sequelize')
 //const sequelize = new Sequelize('mysql://user:secret@localhost:3306/express')
 
 const app = express()
+
+// Parser configs
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 // View configs
 app.engine('handlebars', hbs())
@@ -11,5 +16,6 @@ app.set('view engine', 'handlebars')
 
 // Rotas
 app.get('/', (req, res) => res.render('index'))
+app.post('/add', (req, res) => res.send(req.body))
 
 app.listen(3002)
