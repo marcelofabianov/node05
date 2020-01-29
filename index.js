15aeccaf40160280939108fa348e8094967d14df
Marcelo Fabiano <marcelofabiano@hotmail.com.br>
2020-01-29 11:23:10 -0300

    configs handlebars

--- index.js
+++ index.js
@@ -6,19 +6,10 @@ const app = express()
 const sequelize = new Sequelize('mysql://user:secret@localhost:3306/express')
 
 // View configs
-app.engine(
-    'handlebars',
-    hbs({
-        extname: 'hbs',
-        defaultLayout: 'default',
-        layoutsDir: __dirname + '/views/pages/',
-        partialsDir: __dirname + '/views/partials',
-    })
-)
-
+app.engine('handlebars', hbs())
 app.set('view engine', 'handlebars')
 
 // Rotas
-app.get('/', (req, res) => res.render('index', { nome: 'Marcelo Fabiano' }))
+app.get('/', (req, res) => res.render('index'))
 
 app.listen(3002, () => console.log('Nodemon...'))
