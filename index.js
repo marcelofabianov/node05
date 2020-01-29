4b217c8fea6188e1cd1de620ca99f26013d0ea02
Marcelo Fabiano <marcelofabiano@hotmail.com.br>
2020-01-29 08:36:47 -0300

    layout default

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-const express = require('express');
-const hbs = require('handlebars');
+const express = require('express')
+const hbs = require('handlebars')
+const Sequelize = require('sequelize')
 
-const app = express();
+const app = express()
+const sequelize = new Sequelize('mysql://user:secret@localhost:3306/express')
 
 app.engine(
     'handlebars',
     hbs({
-        defaultLayout: 'main'
+        extname: 'hbs',
+        defaultLayout: 'default',
     })
-);
+)
 
-app.listen(3002, () => console.log('Nodemon'));
+app.listen(3002, () => console.log('Nodemon'))
