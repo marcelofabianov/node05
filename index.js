97e90e51766a0ce50747b8e4294a2f0cd8af5afc
Marcelo Fabiano <marcelofabiano@hotmail.com.br>
2020-01-29 09:30:23 -0300

    layout

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const express = require('express')
-const hbs = require('handlebars')
+const hbs = require('express-handlebars')
 const Sequelize = require('sequelize')
 
 const app = express()
 const sequelize = new Sequelize('mysql://user:secret@localhost:3306/express')
 
+// View configs
 app.engine(
     'handlebars',
     hbs({
         extname: 'hbs',
         defaultLayout: 'default',
+        layoutsDir: __dirname + '/views/pages/',
+        partialsDir: __dirname + '/views/partials',
     })
 )
 
-app.listen(3002, () => console.log('Nodemon'))
+app.set('view engine', 'handlebars')
+
+// Rotas
+app.get('/', (req, res) => res.render('index', { nome: 'Marcelo Fabiano' }))
+
+app.listen(3002, () => console.log('Nodemon...'))
